f6721e08fc6f0553db0fc5cbe18d7eac97ced940
Kenya Guzman <kenyaguzmanp@gmail.com>
2024-01-12 19:59:34 -0300

    Add icons to bottom navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { DrawerNavigator } from './DrawerNavigator';
 import { Contact } from '../screens/Contact/Contact';
 import { Favourites } from '../screens/Favourites/Favourites';
+import HomeIcon from '../SvgIcons/HomeIcon/HomeIcon';
+import { PeopleIcon } from '../SvgIcons/PeopleIcon/PeopleIcon';
 
 interface NavigationProps {}
 
@@ -35,8 +36,26 @@ const Navigation: FC<NavigationProps> = ({}) => {
   return (
     <NavigationContainer theme={MyTheme}>
       <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={StackNavigator} options={{ headerShown: false }} />
-        <Tabs.Screen name="Contact" component={Contact} options={{ headerShown: false }} />
+        <Tabs.Screen
+          name="Home"
+          component={StackNavigator}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => {
+              return <HomeIcon />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => {
+              return <PeopleIcon />;
+            },
+          }}
+        />
       </Tabs.Navigator>
     </NavigationContainer>
   );
